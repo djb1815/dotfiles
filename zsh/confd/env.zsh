72a91f44fb9e96591dc7c68fd60b74b3a45cf56e
Daniel Beckins <djb1815@users.noreply.github.com>
2026-02-01 16:07:44 +0000

    Apply Catppuccin theme to various terminal utilities

--- zsh/confd/env.zsh
+++ zsh/confd/env.zsh
@@ -38,3 +38,10 @@ export FZF_CTRL_R_OPTS="
 export FZF_ALT_C_OPTS="
   --walker-skip .git,node_modules,Library,.Trash
   --preview 'eza --tree --icons=auto --group-directories-first --level=2 {}'"
+
+export FZF_DEFAULT_OPTS=" \
+  --color=bg+:#363A4F,bg:#24273A,spinner:#F4DBD6,hl:#ED8796 \
+  --color=fg:#CAD3F5,header:#ED8796,info:#C6A0F6,pointer:#F4DBD6 \
+  --color=marker:#B7BDF8,fg+:#CAD3F5,prompt:#C6A0F6,hl+:#ED8796 \
+  --color=selected-bg:#494D64 \
+  --color=border:#6E738D,label:#CAD3F5"
