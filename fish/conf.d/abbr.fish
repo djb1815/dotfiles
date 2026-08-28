b02eac92dcdc848a5301bd894c4cbbb8e4192b20
Daniel Beckins <djb1815@users.noreply.github.com>
2026-08-28 21:31:30 +0100

    Cleanup fish aliases/abbrs

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -1,7 +1,6 @@
 abbr --add '~' 'cd ~'
 abbr --add '-' 'cd -'
 abbr --add '..' 'cd ..'
-abbr --add vi nvim
 
 # dirstack navigation
 for i in (seq 1 9)
@@ -10,13 +9,5 @@ end
 
 # typos
 abbr -a -- cd.. "cd .."
-abbr -a -- which type
-
-abbr --add l 'ls --header --long'
-abbr --add lm 'ls --header --long --sort=modified'
-abbr --add ll 'ls --all --header --long'
-abbr --add llm 'ls --all --header --long --sort=modified'
-abbr --add lt 'ls --tree'
-abbr --add tree 'ls --tree'
-abbr --add la 'eza -lbhHigUmuSa'
-abbr --add lx 'eza -lbhHigUmuSa@'
+abbr -a -- cd- "cd -"
+abbr -a lt 'tree'
---
+++ fish/conf.d/aliases.fish
@@ -0,0 +1,10 @@
+alias which 'type' # better results using 'type' builtin
+
+# ls
+alias l 'ls --header --long'
+alias lm 'ls --header --long --sort=modified'
+alias ll 'ls --all --header --long'
+alias llm 'ls --all --header --long --sort=modified'
+alias tree 'ls --tree'
+alias la 'eza -lbhHigUmuSa'
+alias lx 'eza -lbhHigUmuSa@'
