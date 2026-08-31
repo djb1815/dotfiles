c9d1ee8834ecdd48bf5d7fa06e1915a72df619d9
Daniel Beckins <djb1815@users.noreply.github.com>
2026-08-31 16:49:18 +0100

    Initial config of tide
    Preferring pure at the moment, but may come back to this in the future.

---
+++ fish/conf.d/tide-config.fish
@@ -0,0 +1,20 @@
+# left prompt
+set -g tide_left_prompt_items pwd vcs newline character
+set -g tide_left_prompt_frame_enabled true
+set -g tide_left_prompt_prefix ' '
+
+# character
+set -g tide_character_icon '❯❯'
+
+# git
+set -g tide_git_icon ''
+set -g tide_git_branch 'green'
+set -g tide_git_truncation_length 40
+
+# pwd
+set -g tide_pwd_color_dirs 'blue'
+set -g tide_pwd_color_anchors 'blue'
+set -g tide_pwd_color_truncated_dirs 'blue'
+
+# time
+set -g tide_time_format ' %T'
