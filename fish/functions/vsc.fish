6c7af5a790f9b4e76c52b4ae6761a5a60d927b33
Daniel Beckins <djb1815@users.noreply.github.com>
2026-09-04 19:47:44 +0100

    Update vscode fish alias functions to wrap

--- fish/functions/vsc.fish
+++ fish/functions/vsc.fish
@@ -1,4 +1,4 @@
-function vsc
+function vsc --wraps code --description 'opens current directory or specified file in vscode'
     if test (count $argv) -gt 0
         code $argv
     else
--- fish/functions/vscd.fish
+++ fish/functions/vscd.fish
@@ -1,3 +1,3 @@
-function vscd
+function vscd --wraps code --description 'open diff in vscode'
     code --diff $argv
 end
